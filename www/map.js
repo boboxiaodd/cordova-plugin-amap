3b052fc4d7e6379b6320b9d5f34301f6ad6f0b1d
boboxiaodd <iam386@gmail.com>
2022-07-09 09:24:08 +0800

    add

--- www/map.js
+++ www/map.js
@@ -1,16 +1,16 @@
 const exec = require('cordova/exec');
-const CDVPicker = {
+const CDVAMap = {
     open_datepicker:function (success,option){
-        exec(success,null,'CDVPicker','open_datepicker',[option]);
+        exec(success,null,'CDVAMap','open_datepicker',[option]);
     },
     open_datetimepikcer:function (success,option){
-        exec(success,null,'CDVPicker','open_datetimepikcer',[option]);
+        exec(success,null,'CDVAMap','open_datetimepikcer',[option]);
     },
     open_picker:function (success,option){
-        exec(success,null,'CDVPicker','open_picker',[option]);
+        exec(success,null,'CDVAMap','open_picker',[option]);
     },
     open_citypicker:function (success,option){
-        exec(success,null,'CDVPicker','open_citypicker',[option]);
+        exec(success,null,'CDVAMap','open_citypicker',[option]);
     }
 };
-module.exports = CDVPicker;
+module.exports = CDVAMap;
